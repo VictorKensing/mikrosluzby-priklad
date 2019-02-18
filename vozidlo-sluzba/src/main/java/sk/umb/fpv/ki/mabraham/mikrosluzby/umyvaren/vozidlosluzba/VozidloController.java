430d10c4d6e0b9f1b276b779cb05a9a5f17e8087
Mario Abraham <mario.abraham@outlook.com>
2019-02-18 17:12:32 +0100

    pridanie vozidla cez post

--- vozidlo-sluzba/src/main/java/sk/umb/fpv/ki/mabraham/mikrosluzby/umyvaren/vozidlosluzba/VozidloController.java
+++ vozidlo-sluzba/src/main/java/sk/umb/fpv/ki/mabraham/mikrosluzby/umyvaren/vozidlosluzba/VozidloController.java
@@ -1,13 +1,20 @@
 package sk.umb.fpv.ki.mabraham.mikrosluzby.umyvaren.vozidlosluzba;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class VozidloController {
@@ -17,7 +24,7 @@ public class VozidloController {
 	@Autowired
 	private VozidloRepository repository;
 	
-	@GetMapping("/vozidlo")
+	@GetMapping("/vozidla")
 	public List<Vozidlo> retrieveAll() {
 		
 		List<Vozidlo> vozidla = repository.findAll();
@@ -27,7 +34,7 @@ public class VozidloController {
 		return vozidla;
 	}
 	
-	@GetMapping("/vozidlo/{ecv}")
+	@GetMapping("/vozidla/{ecv}")
 	public Vozidlo retrieveVozidlo(@PathVariable String ecv) {
 		
 		Vozidlo vozidlo = repository.findByEcv(ecv);
@@ -35,6 +42,16 @@ public class VozidloController {
 		logger.info("{}", vozidlo);
 		
 		return vozidlo;
+	}
+	
+	@PostMapping("/vozidla")
+	public ResponseEntity<Object> createVozidlo(@Valid @RequestBody Vozidlo vozidlo) {
+		Vozidlo noveVozidlo = repository.save(vozidlo);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{ecv}")
+				.buildAndExpand(noveVozidlo.getEcv()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 
 }
