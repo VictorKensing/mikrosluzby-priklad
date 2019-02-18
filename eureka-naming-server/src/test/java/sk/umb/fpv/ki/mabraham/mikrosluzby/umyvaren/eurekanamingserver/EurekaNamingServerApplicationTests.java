837371b3a36a0300de1d3e1c78ea6cb054a3a769
Mario Abraham <mario.abraham@outlook.com>
2019-02-18 12:00:48 +0100

    nahodenie netflix eureka naming server

---
+++ eureka-naming-server/src/test/java/sk/umb/fpv/ki/mabraham/mikrosluzby/umyvaren/eurekanamingserver/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,16 @@
+package sk.umb.fpv.ki.mabraham.mikrosluzby.umyvaren.eurekanamingserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaNamingServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
